1c6a8d8bfb23e2dd0784e47e8734f6192bc85637
nihalnmp <nihal.mca16.du@gmail.com>
2017-02-04 21:35:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/bootstrap/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/bootstrap', 'bootstrap.min.css'));
+});
+app.get('/ui/bootstrap/jquery-3.1.1.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/bootstrap', 'jquery-3.1.1.min.js'));
+});
+app.get('/ui/bootstrap/bootstrap.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/bootstrap', 'bootstrap.min.js'));
+});
+app.get('/ui/images/photo.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/images', 'photo.jpg'));
+});
+app.get('/ui/profile.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+});
+app.get('/ui/articles.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articles.html'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
