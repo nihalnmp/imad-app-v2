2219e825b2a734a5574a0199d8811b54bbb28d00
nihalnmp <nihal.mca16.du@gmail.com>
2017-02-04 23:05:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,9 @@ app.get('/ui/images/twitter_icon.png', function (req, res) {
 app.get('/ui/images/nihal.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/images', 'nihal.jpg'));
 });
-
+app.get('/ui/images/profile_banner.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/images', 'profile_banner.jpg'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
