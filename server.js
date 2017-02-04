7039f3f7aa1983a488999a5f81263a0914b4a773
nihalnmp <nihal.mca16.du@gmail.com>
2017-02-04 23:11:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,17 +21,19 @@ app.get('/ui/bootstrap/jquery-3.1.1.min.js', function (req, res) {
 app.get('/ui/bootstrap/bootstrap.min.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/bootstrap', 'bootstrap.min.js'));
 });
-app.get('/ui/images/photo.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/images', 'photo.jpg'));
-});
-app.get('/ui/images/banner.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/images', 'banner.png'));
-});
+
 app.get('/ui/profile.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 app.get('/ui/articles.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articles.html'));
+});
+//Images
+app.get('/ui/images/photo.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/images', 'photo.jpg'));
+});
+app.get('/ui/images/banner.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/images', 'banner.png'));
 });
 app.get('/ui/images/fb_icon.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/images', 'fb_icon.png'));
@@ -48,6 +50,7 @@ app.get('/ui/images/nihal.jpg', function (req, res) {
 app.get('/ui/images/profile_banner.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/images', 'profile_banner.jpg'));
 });
+//Images
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
