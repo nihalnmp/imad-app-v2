b680b29a6d68215c79b9f9adae345fc926b64a56
nihalnmp <nihal.mca16.du@gmail.com>
2017-02-21 20:20:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 //request routes
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.php'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
